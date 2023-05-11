5a7811a5306195353d04fc062c7ae20e7c5831e8
Thein Naing <theinnaing0908@gmail.com>
2023-05-11 10:35:36 +0800

    add h4/p/p tags more for rendering person object keys author/job/info with respective className

--- src/Review.js
+++ src/Review.js
@@ -16,6 +16,9 @@ const Review = () => {
         <FaQuoteRight />
       </span>
       </div>
+      <h4 className="author">{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
     </article>
 
   )
