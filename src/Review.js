eb187a7eab23bfe20b180cb4271aaa5ff9b4fd59
Thein Naing <theinnaing0908@gmail.com>
2023-05-11 10:15:00 +0800

    console.log(people) to check people objects array

--- src/Review.js
+++ src/Review.js
@@ -5,6 +5,7 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
   const [index, setIndex] = useState(0);
+  console.log(people)
 
   return (<h2>review component</h2>
 
