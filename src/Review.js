288b06dcecb6366b153f689bd3b051e6a4e6b369
Thein Naing <theinnaing0908@gmail.com>
2023-05-11 09:58:37 +0800

    create our reviews rendering in App.js with main/section/h2/div tags and also render <Review /> component

--- src/Review.js
+++ src/Review.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
-import { FaGithubSquare } from "react-icons/fa";
+
 
 const Review = () => {
   return <h2>review component</h2>;
