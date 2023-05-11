ee4fe1407f8e463043d5be8a74663943df2e1638
Thein Naing <theinnaing0908@gmail.com>
2023-05-11 10:40:02 +0800

    add button/className='prev-btn' inside div/className='button-container' for rendering <FaChvronLeft /> icon component

--- src/Review.js
+++ src/Review.js
@@ -19,6 +19,13 @@ const Review = () => {
       <h4 className="author">{name}</h4>
       <p className='job'>{job}</p>
       <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn'>
+          <FaChevronLeft />
+        </button>
+      </div>
+
+
     </article>
 
   )
